b9950f2e139925a7e3761c7955c49c01819b33d1
hwangseonheon <dolphin33@jbnu.ac.kr>
2023-11-07 22:10:31 +0900

    refactor: 입력 체크 부분 함수화 및 코드 수정

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from three_operations import ThreerBasicOperations
 
 infix= ThreerBasicOperations.make_infix()
+ThreerBasicOperations.infix_check(infix)
 postfix = ThreerBasicOperations.make_postfix(infix)
 result = ThreerBasicOperations.calculate(postfix)
 
--- src/three_operations.py
+++ src/three_operations.py
@@ -17,20 +17,19 @@ class ThreerBasicOperations:
       if userInput == '=':
         break
       infix.append(userInput)
-
-    # 입력 체크
-    op = ""
-    if len(infix) > 1:
-      op = infix[1]
+    return infix
+  
+  def infix_check(infix):
     number_of_inputs_error(len(infix)) # 저장된 값의 개수가 짝수일 때(마지막 입력 숫자 X) Error!
+    firstOp = ""
+    if len(infix) > 1:
+      firstOp = infix[1]
+      correct_opr_error(firstOp) # 첫 번째 연산자 "+", "-", "*" 가 아닐 시 Error!
     for index, item in enumerate(infix):
       if index % 2:
-        correct_opr_error(item) # 홀수 인덱스의 값이 "+", "-", "*" 가 아닐 시 Error!
-        opr_error(op, item) # 사용되는 연산자가 1개 초과 시 Error!
+        opr_error(firstOp, item) # 첫 번째 연산자와 다를 시 Error!
       else:
         val_error(item) # 짝수 인덱스의 값이 정수가 아닐 시 Error!
-    
-    return infix
 
   # 후위표기법 적용 ex) 532*+
   def make_postfix(infix):
